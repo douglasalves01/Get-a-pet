418631623d808cad612728c67b259e1c243bc96a
Douglas Alves <douglasmonttozo@hotmail.com.br>
2023-12-31 19:03:09 -0300

    configuraçaõ inicial

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+//solve cors
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+//public folder for images
+app.use(express.static("public"));
+
+app.listen(5000, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Aplicação rodando em http://localhost:3000");
+});
