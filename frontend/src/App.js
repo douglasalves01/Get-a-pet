6c3a0420b913a0a1c90a9bd04b4067a7bf7a6ea1
Douglas Alves <douglasmonttozo@hotmail.com.br>
2024-01-04 22:13:32 -0300

    inserindo react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Register from "./components/pages/Auth/Register";
+import Login from "./components/pages/Auth/Login";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
